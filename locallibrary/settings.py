f8cc6f6f1cd075d396b6a8c6edc1925c5320633e
siawashkasra <siawashkasra@gmail.com>
2020-11-17 19:51:53 +0430

    Configure postgresql as default database

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'locallibrary',
+        'USER': 'siawashkasra',
+        'PASSWORD': 'kasra@321',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
