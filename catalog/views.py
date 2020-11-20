00986318923c069162b2e29514d9761d2a4ee011
siawashkasra <siawashkasra@gmail.com>
2020-11-20 10:37:40 +0430

    Add generic list and detail feature to view

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
+from django.db.models.base import Model
 from django.shortcuts import render
 from catalog.models import Book, BookInstance, Genre, Author
+from django.views import generic
+
 
 # Create your views here.
 
@@ -18,3 +21,25 @@ def index(request):
     }
 
     return render(request, 'catalog/index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    # context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
