7f47112148563ad069983acf8c481ffbe0da5e78
siawashkasra <siawashkasra@gmail.com>
2020-11-20 14:09:00 +0430

    Add visits count by using session object

--- catalog/views.py
+++ catalog/views.py
@@ -11,13 +11,17 @@ def index(request):
     num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
 
     context = {
 
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_visits': num_visits
     }
 
     return render(request, 'catalog/index.html', context=context)
